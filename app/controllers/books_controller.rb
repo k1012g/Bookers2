75d9544aa592eedf33dfc8f24efecde4ae8f9281
k1012g <k1012g@k1012guramunoMacBook-Pro.local>
2019-11-22 20:27:11 +0900

    app2 task is finished

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,61 @@
+class BooksController < ApplicationController
+	before_action :correct_book, only: [:edit]
+
+	def show
+		@book = Book.new
+		@book_show = Book.find(params[:id])
+		@user = @book_show.user
+	end
+
+	def edit
+		@book = Book.new
+		@book_edit = Book.find(params[:id])
+	end
+
+	def destroy
+		@book_show = Book.find(params[:id])
+		@book_show.destroy
+		redirect_to books_path
+	end
+
+	def update
+		@book_edit = Book.find(params[:id])
+		if 	@book_edit.update(book_params)
+			flash[:notice] = "You have updated book successfully."
+			redirect_to book_path
+		else
+			render :edit
+		end
+	end
+
+	def create
+		@book = Book.new(book_params)
+		@book.user_id = current_user.id
+		if @book.save
+			flash[:notice] = "You have created book successfully."
+			redirect_to book_path(@book.id)
+		else
+			@books = Book.all
+			@user = current_user
+			render :index
+		end
+	end
+
+	def index
+		@book = Book.new
+		@books = Book.all
+		@user = current_user
+	end
+
+	private
+	def book_params
+		params.require(:book).permit(:title, :body)
+	end
+
+	def correct_book
+		@book = Book.find(params[:id])
+		if @book.user_id != current_user.id
+			redirect_to books_path
+		end
+	end
+end
---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '942a29a15c3b114a08a8a45af7d170a57c74672d567c364a4f99698a6334931851abc3bc5070bce6a1286c5a1cef582e0d3356c6bffc373eabd1e90a96d903a4'
